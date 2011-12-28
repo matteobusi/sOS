90110327fd7b3ad4818e7600f874a3810953fa0a
WinSid <caos@winsid.net>
2011-12-28 13:49:55 +0100

    README Updated

--- src/headers/panic.h
+++
@@ -1,8 +0,0 @@
-#ifndef PANIC_H
-#define	PANIC_H
-
-//Panic functions
-void kpanic(char* error_string, int address_violation, char* file, int line);
-void kexception(int err_no, int int_no);
-#endif	/* PANIC_H */
-
